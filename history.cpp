a65d57c9bfe59d42cd2101ee95b5931d6a62c369
O-xix <ichorlifephoenix@gmail.com>
2025-03-12 02:30:33 -0700

    History has now been completed, would require better implementation of Customer.printName(), Customer.getCustomer(), Customer.displayHistory().

--- history.cpp
+++ history.cpp
@@ -7,16 +7,17 @@
 
 
 void History::process(Store* store) {
-    //note: this is a placeholder method, because the customer history is not actually processed, but rather printed
-    cout << ">> Customer " << customer_id << " history requested" << endl;
-}
-
-void History::print(Store* store) {
-    // @todo: There's no way to get the customer object from the customer_id
     Customer* customer = store->getCustomer(customer_id);
     if (customer == nullptr) {
-        cout << "Customer not found" << endl;
+        cout << ">> Customer " << customer_id << " not found." << endl;
         return;
     }
-    cout << "History for " << customer->getName() << endl;
+    cout << ">> Customer " << customer_id << " History (H) requested: " << endl;
+    
+    cout << "History for ";
+    customer->printName();
+    cout << ":" << endl;
+
+    customer->displayHistory();
+    cout << endl;
 }
--- history.h
+++ history.h
@@ -7,12 +7,12 @@
 
 #include "transaction.h"
 #include "store.h"
+#include "customer.h"
 
 class History : public Transaction {
 public:
     History(int customer_id) : transaction_type('H'), customer_id(customer_id) {}; //Default Action constructor
     void process(Store* store); //Inherited method to process the type of transaction where this method would be called, if ever it needed to be. Can be overloaded with additional parameters if necessary for a transaction.
-    void print(Store* store);
 private:
     char transaction_type; //Stores the character representing the transaction type in the input file, which is the only field the transactions have in common; all other fields exist in sub-/child classes.
     int customer_id;
