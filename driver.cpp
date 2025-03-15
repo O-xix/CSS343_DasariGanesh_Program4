39131eb6e6f77afc7eab723178aaf9bf60de1753
O-xix <ichorlifephoenix@gmail.com>
2025-03-15 12:54:41 -0700

    Console parameters work.

--- driver.cpp
+++ driver.cpp
@@ -9,30 +9,28 @@ using namespace std;
 #include "store.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
 //int main(int argc, char *argv[]) {
     // Requires three files, in this order: Movies, Customers, Commands
-    /*
+
+    Store store;
+    
     if (argc != 4) {
       cerr << "Usage: " << argv[0] << " <movie.txt-file> <customer.txt-file> <commands.txt-file>" << endl;
     }
-    ifstream movieFile;
-    movieFile.open(argv[1]);
-    ifstream customerFile;
-    customerFile.open(argv[2]);
-    ifstream commandsFile;
-    commandsFile.open(argv[3]);
-    */
+    store.initInventory(argv[1]);//lol venom
+    store.initCustomers(argv[2]);//hi
+    store.processCommands(argv[3]);
 
     // Have a static way of grabbing the files
     
     // Create new Store
-    Store store;
+    //Store store;
 
     //"data4movies.txt", using absolute file paths here because CMAKE is horrible with understanding what's in your directory
-    store.initInventory("data4movies.txt");//lol venom
-    store.initCustomers("data4customers.txt");//hi
-    store.processCommands("data4commands.txt");
+    //store.initInventory("data4movies.txt");//lol venom
+    //store.initCustomers("data4customers.txt");//hi
+    //store.processCommands("data4commands.txt");
 
 
     //Testing PR
